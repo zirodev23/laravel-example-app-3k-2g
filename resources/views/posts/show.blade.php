de0a6e919da00ebac8dafc47eb555ff07d7dd647
unknown <your@email.example>
2024-10-21 12:20:53 +0300

    link to all posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Single post view</h1>
-    <p>Title: {{ $post->title}}</p>
-    <p>Content: {{ $post->content}}</p>
+    <p>Title: {{ $post->title }}</p>
+    <p>Content: {{ $post->content }}</p>
+
+    <a href="/posts">Back To All Posts</a>
+
 </body>
+
 </html>
